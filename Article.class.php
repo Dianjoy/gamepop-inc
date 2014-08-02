b7c51e65cb97cc7a0b599135b3f1944e9214da2b
Meathill <meathill@gmail.com>
2014-08-02 14:52:27 +0800

    修改读取文章附加信息的逻辑

--- Article.class.php
+++ Article.class.php
@@ -12,6 +12,7 @@ class Article extends \gamepop\Base {
   const CATEGORY = '`t_article_category`'; // 咳咳，这个和下面那个刚好反过来，请注意
   const CATEGORY_IMAGE = '`t_article_category_image`';
   const ARTICLE_CATEGORY = '`t_category`';
+  const TOP = '`t_article_top`';
 
   const NORMAL = 0;
   const DELETED = 1;
@@ -21,7 +22,8 @@ class Article extends \gamepop\Base {
   static $ALL = "`t_article`.`id`, `guide_name`, `category`, `label`, `source`,
     `topic`, `author`, `t_article`.`icon_path`, `pub_date`, `src_url`, `seq`, `remark`,
     `update_time`, `update_editor`, `is_top`, `is_index`, `t_article`.`status`";
-  static $TOP = "`id`, `topic`, `update_time`, `seq`, `is_top`, `icon_path`, `source`, `author`";
+  static $TOP = "`aid`, `t_article_top`.`id`, `topic`, `start_time`, `end_time`,
+   `t_article_top`.`seq`, `icon_path`";
   static $DETAIL = "`guide_name`, `category`, `label`, `source`,
     `topic`, `author`, `icon_path`, `content`, `remark`, `pub_date`, `src_url`,
      `seq`, `update_time`, `update_editor`, `t_article`.`status`, `is_top`";
@@ -79,6 +81,78 @@ class Article extends \gamepop\Base {
       ->fetchALL(PDO::FETCH_ASSOC);
   }
 
+  public function fetch_meta_data($articles) {
+    // 取出各种数据
+    $ids = array();
+    $editors = array();
+    $guide_names = array();
+    foreach ($articles as $item) {
+      $ids[] = $item['id'];
+      $guide_names[] = $item['guide_name'];
+      if ($item['update_editor']) {
+        $editors[] = $item['update_editor'];
+      }
+    }
+    $guide_names = array_unique($guide_names);
+    $editors = array_unique($editors);
+
+    // 读取分类
+    $category = $this->select(Article::$CATEGORY)
+      ->where(array('aid' => $ids), '', gamepop\Base::R_IN)
+      ->fetchAll(PDO::FETCH_ASSOC);
+    $cates = array();
+    foreach ($category as $item) {
+      $item['id'] = $item['cid'];
+      if (isset($cates[$item['aid']])) {
+        $cates[$item['aid']][] = $item;
+      } else {
+        $cates[$item['aid']] = array($item);
+      }
+    }
+
+
+    // 读取作者，用作者名取代标记
+    if (count($editors)) {
+      require_once "../../inc/Admin.class.php";
+      $admin = new Admin();
+      $editors = $admin->select(Admin::$BASE)
+        ->where(array('id' => $editors), '', \gamepop\Base::R_IN)
+        ->fetchAll(PDO::FETCH_COLUMN | PDO::FETCH_UNIQUE);
+      foreach ($articles as $key => $article) {
+        $articles[$key]['update_editor'] = $editors[$article['update_editor']];
+      }
+    }
+
+    // 读取关联游戏
+    if (count($guide_names)) {
+      require_once "../../inc/Game.class.php";
+      $game = new Game();
+      $games = $game->select(Game::$ALL)
+        ->where(array('guide_name' => $guide_names), '', \gamepop\Base::R_IN)
+        ->fetchAll(PDO::FETCH_ASSOC | PDO::FETCH_UNIQUE);
+    }
+
+    // 读取置顶状态
+    $now = date('Y-m-d H:i:s');
+    $top = $this->select(Article::$TOP)
+      ->where(array('aid' => $ids), '', \gamepop\Base::R_IN)
+      ->where(array('status' => 0), Article::TOP)
+      ->where(array('end_time' => $now), '', \gamepop\Base::R_MORE_EQUAL)
+      ->fetchAll(PDO::FETCH_ASSOC | PDO::FETCH_UNIQUE);
+
+    // 补全数据
+    foreach ($articles as $key => $item) {
+      $item['category'] = (array)$cates[$item['id']];
+      $item['game_name'] = $games[$item['guide_name']]['game_name'];
+      $item['is_top'] = (int)$item['is_top'];
+      $item['status'] = (int)$item['status'];
+      $item['top'] = (int)isset($top[$item['id']]);
+      $articles[$key] = $item;
+    }
+
+    return $articles;
+  }
+
   protected function getTable($fields) {
     if (is_string($fields)) {
       if (strpos($fields, self::$ALL) !== false || strpos($fields, self::$DETAIL) !== false) {
@@ -92,6 +166,9 @@ class Article extends \gamepop\Base {
       }
       if ($fields === self::$CATEGORY) {
         return self::CATEGORY . " RIGHT JOIN " . self::ARTICLE_CATEGORY . " ON " . self::CATEGORY . ".`id`=" . self::ARTICLE_CATEGORY . ".`cid`";
+      }
+      if ($fields === self::$TOP) {
+        return self::TOP . " LEFT JOIN " . self::TABLE . " ON " . self::TOP . ".`aid`=" . self::TABLE . ".`id`";
       }
     }
     if (is_array($fields)) {
